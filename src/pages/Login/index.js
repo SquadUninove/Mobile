584996a8e933217bf3aae2e309e8652d9f02ae55
Gustavo <gustavomarianosoares@gmail.com>
2021-10-26 18:48:04 -0300

    Base da API com Token

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -6,11 +6,37 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  AsyncStorage,
+  Alert,
 } from "react-native";
 import { style } from "../../components/background";
 import { estiloLogin } from "./styles";
+import api from "../../services/api";
 
 export default function Login({ navigation }) {
+  state = {
+    errorMessage: null,
+  };
+
+  signIn = async () => {
+    try {
+      const response = await api.post("", {
+        email: "",
+        password: "",
+      });
+
+      const { user, token } = response.data;
+
+      await AsyncStorage.multiSet([
+        ["@CodeApi:token", token],
+        ["@CodeApi:user", JSON.stringify(user)],
+      ]);
+      Alert.alert("Login com sucesso!");
+    } catch (response) {
+      this.setState({ errorMessage: response.data.error });
+    }
+  };
+
   return (
     <SafeAreaView style={style.background}>
       <View>
@@ -29,9 +55,11 @@ export default function Login({ navigation }) {
         <TouchableOpacity
           style={estiloLogin.botaoAzul}
           onPress={() => navigation.navigate("Qr")}
+          onPress={this.signIn}
         >
           <Text style={estiloLogin.textoBotao}>Entrar</Text>
         </TouchableOpacity>
+        {!!this.state.errorMessage && <Text>{this.state.errorMessage}</Text>}
 
         <Text style={estiloLogin.textoNormal}>ou</Text>
 
--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,8 @@ const api = axios.create({
   baseURL: "https://salty-gorge-92638.herokuapp.com/",
 });
 
+api.addResponseTransform((response) => {
+  if (!response.ok) throw response;
+});
+
 export default api;
